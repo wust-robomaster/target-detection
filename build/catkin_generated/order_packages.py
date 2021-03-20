00a73269ccfa89f1c0bfb5633040fe6f458f4f9f
wust-robomaster <81006487+wust-robomaster@users.noreply.github.com>
2021-03-20 18:30:41 +0800

    Add files via upload

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/xutao/fcg_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/xutao/catkin_ws/devel;/home/xutao/roborts_ws/devel;/home/xutao/rplidar_ws/devel;/home/xutao/navigation_ws/devel;/opt/ros/melodic'.split(';') if '/home/xutao/catkin_ws/devel;/home/xutao/roborts_ws/devel;/home/xutao/rplidar_ws/devel;/home/xutao/navigation_ws/devel;/opt/ros/melodic' != '' else []
