cd2784c54d10915db3d0abb84f63fa95f0e33a97
wust-robomaster <81006487+wust-robomaster@users.noreply.github.com>
2021-03-20 18:26:21 +0800

    Add files via upload

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/xutao/fcg_ws/src
+# Build directory: /home/xutao/fcg_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("my_roborts_camera")
